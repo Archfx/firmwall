e957c15ca5cdbf0064603f4a270291878ba976ce
Archfx <cybx@live.com>
2024-06-20 17:05:01 -0400

    script structure changed

---
+++ binaries/test/tfm_ipc/bin/zephyr.dts.d
@@ -0,0 +1,8 @@
+empty_file.o: /home/aruna/Documents/Zephyr/zephyr/misc/empty_file.c \
+ /home/aruna/Documents/Zephyr/zephyr/boards/arm/mps2/mps2_an521_cpu0_ns.dts \
+ /home/aruna/Documents/Zephyr/zephyr/dts/common/mem.h \
+ /home/aruna/Documents/Zephyr/zephyr/dts/arm/armv8-m.dtsi \
+ /home/aruna/Documents/Zephyr/zephyr/dts/common/skeleton.dtsi \
+ /home/aruna/Documents/Zephyr/zephyr/include/zephyr/dt-bindings/i2c/i2c.h \
+ /home/aruna/Documents/Zephyr/zephyr/include/zephyr/dt-bindings/input/input-event-codes.h \
+ /home/aruna/Documents/Zephyr/zephyr/boards/arm/mps2/mps2_an521-common.dtsi
--- run.sh
+++ run.sh
@@ -4,17 +4,15 @@ setup_venv(){
 	./scripts/setup.sh
 }
 
-activate_venv() {
-	source .venv/bin/activate
-}
+# activate_venv() {
+# 	source .venv/bin/activate
+# }
 
-run_read(){
-	./src/list_func binaries/tfm_integration/tfm_ipc/bin/zephyr.elf
-	# ./src/patch_ex 
-}
+# run_read(){
+# 	./src/list_func binaries/tfm_integration/tfm_ipc/bin/zephyr.elf
+# 	# ./src/patch_ex 
+# }
 
 # Call the function
 setup_venv
-activate_venv
-make
-# run_read
+make -C scripts
